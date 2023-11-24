b6a2b65e5081c76452d67d1370186d26ffee4f1f
lawrence <lordlawrencesalazar@gmail.com>
2023-11-24 17:39:31 +0800

    made minor changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,28 @@
+import Login from "./Login/Login";
+
 function Header() {
   return (
     <div className="Header-container">
       <div className="Header-Menu">
-        <div className="Header-logo">NDSCP</div>
-        <ul className="Menu">
-          <li>
-            <a className="active" href="/">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/news">News</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-        </ul>
+        <div className="Header-Menu-Container">
+          <div className="Header-logo">NDSCP</div>
+          <ul className="Menu">
+            <li>
+              <a className="active" href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/news">News</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+        </div>
+        <div className="Header-auth">
+          <Login />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <a href="/">Logout</a>;
+}
+
+export default Login;
